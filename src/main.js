8c2f07cbfdd236b0afb872e02b878d8ac684078e
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-10 13:47:29 -0300

    organizando pastas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+import javascriptLogo from '../public/JavaScript.svg'
 import viteLogo from '../public/vite.svg'
 import { setupCounter } from './counter.js'
 
