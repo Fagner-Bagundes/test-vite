f52b446df1d03305957ad2436b04839bc5c0f100
FagnerFerreira <ferreirabagundes@gmail.com>
2025-01-10 12:50:40 -0300

    primeiro commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite"
+export default defineConfig({
+    root: './src',
+    base: '/',
+    server: {
+        port :3000,
+    },
+    build: {
+        outDir: '../dist'
+    }
+});
